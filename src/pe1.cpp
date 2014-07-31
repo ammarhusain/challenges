87e7fc30de9e8e07142fc59fdae35014f0128bc3
ahusain <ahusain@nrec-014710.(none)>
2014-07-31 17:31:13 -0400

    Boiler plate repository with sample executable

---
+++ src/pe1.cpp
@@ -0,0 +1,32 @@
+/** ----------------------------------------------------------------------
+ * Copyright 2014 < Ammar Husain (Carnegie Mellon University) > 
+ * 
+ * @file   pe1.cpp
+ * @author Ammar Husain <ahusain@nrec.ri.cmu.edu> 
+ * @date   Thu Jul 31 17:18:28 2014
+ * 
+ * @brief  Project Euler #1
+ * 
+ * 
+ ---------------------------------------------------------------------- */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
+#include <iostream>
+
+/** ----------------------------------------------------------------
+ * Main Routine
+ * 
+ * @param argc 
+ * @param argv 
+ * 
+ * @return 
+ ---------------------------------------------------------------- */
+int main(int argc, char *argv[]) {
+    std::cout << "Hello World!" << std::endl;
+    
+    return 0;
+}
+
