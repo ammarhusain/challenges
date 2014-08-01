26fa8dab249a9bee132b9ebd2d0a26f922eca145
ammarhusain <mrahusain@gmail.com>
2014-08-01 16:12:49 -0400

    implemented problem #2

--- src/pe2.cpp
+++ src/pe2.cpp
@@ -17,6 +17,24 @@
 #include <stdint.h>
 #include <iostream>
 
+void recursiveFibonacci(uint64_t l1, uint64_t l2,
+                            uint64_t& sum, const uint64_t& n) {
+
+    uint64_t newNum = l1 + l2;
+
+    if (newNum > n)
+        return;
+
+    if (newNum%2==0)
+        sum += newNum;
+
+    /// call recursively
+    recursiveFibonacci(newNum, l1, sum, n);
+
+    return;
+    
+}
+
 
 /** ----------------------------------------------------------------
  * Main Routine
@@ -34,8 +52,16 @@ int main(int argc, char *argv[]) {
     
     for (uint i = 0; i < numTests; i++) {
         std::cin >> input;
+
+        uint64_t sum;
+        /// initialize to 2 (sum of evens between 1 & 2)
+        sum = 2;
         
         /// do work here
+        recursiveFibonacci(2, 1, sum, input);
+
+        std::cout << sum << std::endl;
+        
     }
 
     return 0;
