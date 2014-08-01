a7748de063ee34b6ecf07fc29f8a22d0aa71513d
ammarhusain <mrahusain@gmail.com>
2014-08-01 15:52:36 -0400

    added boiler plate main routine

---
+++ src/boiler-plate.cpp
@@ -0,0 +1,43 @@
+/** ----------------------------------------------------------------------
+ * Copyright 2014 < Ammar Husain (Carnegie Mellon University) > 
+ * 
+ * @file   pe1.cpp
+ * @author Ammar Husain <ahusain@nrec.ri.cmu.edu> 
+ * @date   Thu Jul 31 17:18:28 2014
+ * 
+ * @brief  Boiler Plate
+ * 
+ * 
+ ---------------------------------------------------------------------- */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
+#include <stdint.h>
+#include <iostream>
+
+
+/** ----------------------------------------------------------------
+ * Main Routine
+ * 
+ * @param argc 
+ * @param argv 
+ * 
+ * @return 
+ ---------------------------------------------------------------- */
+int main(int argc, char *argv[]) {
+    uint numTests;
+    std::cin >> numTests;
+
+    uint64_t input;
+    
+    for (uint i = 0; i < numTests; i++) {
+        std::cin >> input;
+        
+        /// do work here
+    }
+
+    return 0;
+}
+
