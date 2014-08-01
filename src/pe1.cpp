d99b8c87a83ba4223f5c2f5f2945e87b3281e334
ahusain <ahusain@nrec-014710.(none)>
2014-08-01 09:21:13 -0400

    added O(1) computation using arithmetic progression; still a bug somewhere

--- src/pe1.cpp
+++ src/pe1.cpp
@@ -26,15 +26,41 @@
  * 
  * @return 
  ---------------------------------------------------------------------- */
-int sumMultiples(int input) {
-    int sum(0);
+uint sumMultiplesBrute(uint input) {
+    uint sum(0);
     
-    for (int i = 0; i < input; i++) {
+    for (uint i = 0; i < input; i++) {
         if ((i%3 == 0)||(i%5 == 0))
             sum+=i;
     }
     return sum;
 }
+
+
+/** ----------------------------------------------------------------------
+ * Uses arithmetic progression to compute the sum of multiples
+ * 
+ * @author Ammar Husain <ahusain@nrec.ri.cmu.edu>
+ * @date 07/31/2014 
+ * @param multiple 
+ * @param input 
+ * 
+ * @return 
+ ---------------------------------------------------------------------- */
+uint arithmeticProgressionSum(uint multiple, uint input) {
+    /// compute the number of multiples
+    uint n =
+        uint(ceil(static_cast<double>(input)/static_cast<double>(multiple)) - 1.0);
+    /// get the last number in sequence
+    uint last = n*multiple;
+    /// compute AP sum
+    uint sum = n*(multiple+last)/2;
+
+    std::cout << "m: " << multiple << " i: " << input << " s: " << sum << " n: " << n << std::endl;
+    
+    return sum;
+}
+
 
 /** ----------------------------------------------------------------
  * Main Routine
@@ -45,22 +71,18 @@ int sumMultiples(int input) {
  * @return 
  ---------------------------------------------------------------- */
 int main(int argc, char *argv[]) {
-    int numTests;
+    uint numTests;
     std::cin >> numTests;
 
-    int input, sum;
+    uint input;
     
-    for (int i = 0; i < numTests; i++) {
+    for (uint i = 0; i < numTests; i++) {
         std::cin >> input;
         /// compute the actual sum
-        /// std::cout << sumMultiples(input) << std::endl;
-        sum = 0;
-        for (int i = 0; i < input; i++) {
-            if ((i%3 == 0)||(i%5 == 0))
-                sum+=i;
-        }
-        /// return sum;
+        uint sum = arithmeticProgressionSum(3, input) + arithmeticProgressionSum(5, input) - arithmeticProgressionSum(15, input);
         std::cout << sum << std::endl;
+        std::cout << "brute: " << sumMultiplesBrute(input) << std::endl;
+
     }
 
     return 0;
